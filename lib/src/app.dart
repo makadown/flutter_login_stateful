cb30f1470c8f5f90f59c71e381940505ea33df94
Makadown <maseflores79@gmail.com>
2019-06-15 18:20:22 -0500

    Recreando main app

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build (context) {
+    return MaterialApp(
+      title: 'Log Me In!',
+      home: Scaffold(body: Text('Show a form here!'),),
+    );
+  }
+}
