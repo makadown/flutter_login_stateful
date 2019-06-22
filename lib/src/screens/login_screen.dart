cde1554c22918a7d4bf4dc46b55f64686f34b4b4
Makadown <maseflores79@gmail.com>
2019-06-22 15:44:15 -0500

    Dando forma a nuestro widget de login

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-      createState() {
-        return LoginScreenState();
-      }
+  createState() {
+    return LoginScreenState();
+  }
 }
 
 class LoginScreenState extends State<LoginScreen> {
-  Widget build() {
-    
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [emailField(), passwordField(), submitButton()],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email address',
+        hintText: 'Ej. you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Escribe tu password...',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Color(0xFF42A5F5),
+      onPressed: () {},
+      child: const Text(
+        'Login',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
   }
 }
